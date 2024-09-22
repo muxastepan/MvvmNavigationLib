f897439438e0b2dd9f78ba48b09e954a9b0c97b9
muxastepan <58739030+muxastepan@users.noreply.github.com>
2024-09-22 13:26:27 +0400

    fix: зависимость с захватом в создании сервиса навигации

--- MvvmNavigationLib/Services/ServiceCollectionExtensions/AddNavigationServiceExtensions.cs
+++ MvvmNavigationLib/Services/ServiceCollectionExtensions/AddNavigationServiceExtensions.cs
@@ -2,51 +2,50 @@
 using Microsoft.Extensions.DependencyInjection;
 using MvvmNavigationLib.Stores;
 
-namespace MvvmNavigationLib.Services.ServiceCollectionExtensions
+namespace MvvmNavigationLib.Services.ServiceCollectionExtensions;
+
+public static class AddNavigationServiceExtensions
 {
-    public static class AddNavigationServiceExtensions
-    {
-        public static IServiceCollection AddParameterNavigationService<TViewModel,TNavigationStore,TParam>(
-            this IServiceCollection serviceCollection,
-            Func<IServiceProvider, CreateViewModel<TViewModel,TParam>> viewModelFactory)
-            where TViewModel : ObservableObject 
-            where TNavigationStore : INavigationStore=>
-            serviceCollection.AddSingleton(
-                    services => new ParameterNavigationService<TViewModel, TParam>(
-                        services.GetRequiredService<TNavigationStore>(), 
-                        services.GetRequiredService<CreateViewModel<TViewModel, TParam>>()))
-                .AddSingleton(viewModelFactory);
+    public static IServiceCollection AddParameterNavigationService<TViewModel,TNavigationStore,TParam>(
+        this IServiceCollection serviceCollection,
+        Func<IServiceProvider, CreateViewModel<TViewModel,TParam>> viewModelFactory)
+        where TViewModel : ObservableObject 
+        where TNavigationStore : INavigationStore=>
+        serviceCollection.AddSingleton(
+                services => new ParameterNavigationService<TViewModel, TParam>(
+                    services.GetRequiredService<TNavigationStore>(), 
+                    services.GetRequiredService<CreateViewModel<TViewModel, TParam>>()))
+            .AddSingleton(viewModelFactory);
 
-        public static IServiceCollection AddNavigationService<TViewModel, TNavigationStore>(
-            this IServiceCollection serviceCollection,
-            Func<IServiceProvider,TViewModel> viewModelFactory)
-            where TViewModel : ObservableObject 
-            where TNavigationStore : INavigationStore =>
-            CreateNavigationService<TViewModel,TNavigationStore>(serviceCollection)
-                .AddTransient(viewModelFactory);
+    public static IServiceCollection AddNavigationService<TViewModel, TNavigationStore>(
+        this IServiceCollection serviceCollection,
+        Func<IServiceProvider,TViewModel> viewModelFactory)
+        where TViewModel : ObservableObject 
+        where TNavigationStore : INavigationStore =>
+        CreateNavigationService<TViewModel,TNavigationStore>(serviceCollection)
+            .AddTransient(viewModelFactory);
 
-        public static IServiceCollection AddNavigationService<TViewModel, TNavigationStore>(
-            this IServiceCollection serviceCollection)
-            where TViewModel : ObservableObject 
-            where TNavigationStore : INavigationStore =>
-            CreateNavigationService<TViewModel,TNavigationStore>(serviceCollection)
-                .AddTransient<TViewModel>();
+    public static IServiceCollection AddNavigationService<TViewModel, TNavigationStore>(
+        this IServiceCollection serviceCollection)
+        where TViewModel : ObservableObject 
+        where TNavigationStore : INavigationStore =>
+        CreateNavigationService<TViewModel,TNavigationStore>(serviceCollection)
+            .AddTransient<TViewModel>();
 
-        public static IServiceCollection AddUtilityNavigationServices<TNavigationStore>(
-            this IServiceCollection serviceCollection)
-            where TNavigationStore : INavigationStore =>
-            serviceCollection.AddSingleton<GoBackNavigationService<TNavigationStore>>()
-                .AddSingleton<CloseNavigationService<TNavigationStore>>();
+    public static IServiceCollection AddUtilityNavigationServices<TNavigationStore>(
+        this IServiceCollection serviceCollection)
+        where TNavigationStore : INavigationStore =>
+        serviceCollection.AddSingleton<GoBackNavigationService<TNavigationStore>>()
+            .AddSingleton<CloseNavigationService<TNavigationStore>>();
             
 
-        private static IServiceCollection CreateNavigationService<TViewModel, TNavigationStore>(
-            this IServiceCollection serviceCollection)
-            where TViewModel : ObservableObject 
-            where TNavigationStore : INavigationStore =>
-            serviceCollection.AddSingleton(
-                services => new NavigationService<TViewModel>(
-                        services.GetRequiredService<TNavigationStore>(),
-                    services.GetRequiredService<TViewModel>));
+    private static IServiceCollection CreateNavigationService<TViewModel, TNavigationStore>(
+        this IServiceCollection serviceCollection)
+        where TViewModel : ObservableObject 
+        where TNavigationStore : INavigationStore =>
+        serviceCollection.AddTransient(
+            services => new NavigationService<TViewModel>(
+                services.GetRequiredService<TNavigationStore>(),
+                services.GetRequiredService<TViewModel>));
 
-    }
 }
